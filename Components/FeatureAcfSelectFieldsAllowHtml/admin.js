e63668ecd1fbb12ce29b952ba4bfcbb8edbc32ec
Timo Hubois <info@pixelsaft.de>
2024-04-02 23:17:36 +0200

    feat(FeatureAcfSelectFieldsAllowHtml): use new acf filter

--- Components/FeatureAcfSelectFieldsAllowHtml/admin.js
+++ Components/FeatureAcfSelectFieldsAllowHtml/admin.js
@@ -1,29 +1,18 @@
-/* global acf, jQuery */
+/* global acf */
 
 /**
- * If the field name or key is included in the `acfSelectFieldsToAllowHtml` array,
- * a custom template is used for the dropdown selection that allows HTML content.
+ * Available since ACF 6.2.8
+ * See: https://www.advancedcustomfields.com/resources/javascript-api/#filters-select2_escape_markup
  *
- * @param {Object} options - The original Select2 options.
- * @param {jQuery} $select - The jQuery object for the Select2 dropdown.
- * @param {Object} data - The data for the Select2 dropdown.
- * @param {Object} field - The field data from ACF.
- *
- * @returns {Object} The modified Select2 options.
+ * If the field name or key is included in the `acfSelectFieldsToAllowHtml` array, the value will not be escaped.
  */
-acf.addFilter('select2_args', function (options, $select, data, field) {
-  const { key } = field.data
-  const { name } = field.data
-  if (window.FeatureAcfSelectFieldsAllowHtml.includes(key) || window.FeatureAcfSelectFieldsAllowHtml.includes(name)) {
-    options.templateSelection = function (selection) {
-      const $ = jQuery
-      const $selection = $('<span class="acf-selection"></span>')
-      $selection.html(acf.escHtml(selection.text))
-      $selection.data('element', selection.element)
+acf.add_filter('select2_escape_markup', function (escapedValue, originalValue, $select, settings, field, instance) {
+  const key = field.data('key')
+  const name = field.data('name')
 
-      return $selection
-    }
+  if (window.FeatureAcfSelectFieldsAllowHtml.includes(key) || window.FeatureAcfSelectFieldsAllowHtml.includes(name)) {
+    return originalValue
   }
 
-  return options
+  return escapedValue
 })

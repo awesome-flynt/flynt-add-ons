4f28c9cc72a6db644af1504d0c8d84391bfa7771
Timo Hubois <info@pixelsaft.de>
2024-06-05 10:58:53 +0200

    feat(FeatureAcfSelectFieldsAllowHtml): add filter only when acf is defined

--- Components/FeatureAcfSelectFieldsAllowHtml/admin.js
+++ Components/FeatureAcfSelectFieldsAllowHtml/admin.js
@@ -1,18 +1,21 @@
 /* global acf */
 
-/**
- * Available since ACF 6.2.8
- * See: https://www.advancedcustomfields.com/resources/javascript-api/#filters-select2_escape_markup
- *
- * If the field name or key is included in the `acfSelectFieldsToAllowHtml` array, the value will not be escaped.
- */
-acf.add_filter('select2_escape_markup', function (escapedValue, originalValue, $select, settings, field, instance) {
-  const key = field.data('key')
-  const name = field.data('name')
+if (typeof acf !== 'undefined') {
 
-  if (window.FeatureAcfSelectFieldsAllowHtml.includes(key) || window.FeatureAcfSelectFieldsAllowHtml.includes(name)) {
-    return originalValue
-  }
+  /**
+   * Available since ACF 6.2.8
+   * See: https://www.advancedcustomfields.com/resources/javascript-api/#filters-select2_escape_markup
+   *
+   * If the field name or key is included in the `acfSelectFieldsToAllowHtml` array, the value will not be escaped.
+   */
+  acf.add_filter('select2_escape_markup', function (escapedValue, originalValue, $select, settings, field, instance) {
+    const key = field.data('key')
+    const name = field.data('name')
 
-  return escapedValue
-})
+    if (window.FeatureAcfSelectFieldsAllowHtml.includes(key) || window.FeatureAcfSelectFieldsAllowHtml.includes(name)) {
+      return originalValue
+    }
+
+    return escapedValue
+  })
+}

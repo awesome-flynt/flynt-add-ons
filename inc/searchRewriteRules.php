17c94792f82377ddeaff446c05daa7cd12ffe14c
Timo Hubois <info@pixelsaft.de>
2024-07-08 02:49:04 +0200

    refactor(searchRewriteRules.php): improve search rewrite rules

--- inc/searchRewriteRules.php
+++ inc/searchRewriteRules.php
@@ -6,8 +6,24 @@
 
 namespace Flynt\SearchRewriteRules;
 
+const SEARCH_SLUG = 'search';
+
 add_filter('search_rewrite_rules', function (array $searchRewrite): array {
-    // Make search results page accessible via /search/ instead of /?s=
-    $searchRewrite['search/?$'] = 'index.php?s=';
-    return $searchRewrite;
+    $searchSlug = SEARCH_SLUG;
+
+    $newSearchRewriteRules = [
+        // Pagination rule needs to be added first.
+        "$searchSlug/page/([0-9]{1,})/?$" => 'index.php?s=&paged=$matches[1]',
+        // Make sure that an empty search is handled with the search slug.
+        "$searchSlug/?$" => 'index.php?s=',
+    ];
+
+    // Replace the search slug in the rewrite rules, but keep the original search rewrite rules.
+    foreach ($searchRewrite as $key => $value) {
+        if (!isset($newSearchRewriteRules[$key])) {
+            $newSearchRewriteRules[str_replace('search', $searchSlug, $key)] = $value;
+        }
+    }
+
+    return $newSearchRewriteRules;
 });

8f971642c6c0ac0442ddc7c8e055ee5296696b0c
Timo Hubois <info@pixelsaft.de>
2024-07-08 09:51:31 +0200

    refactor(searchRewriteRules.php): use native $wp_rewrite

--- inc/searchRewriteRules.php
+++ inc/searchRewriteRules.php
@@ -6,22 +6,21 @@
 
 namespace Flynt\SearchRewriteRules;
 
-const SEARCH_SLUG = 'search';
-
 add_filter('search_rewrite_rules', function (array $searchRewrite): array {
-    $searchSlug = SEARCH_SLUG;
+    global $wp_rewrite;
+    $searchBase = $wp_rewrite->search_base;
+    $paginationBase = $wp_rewrite->pagination_base;
 
     $newSearchRewriteRules = [
         // Pagination rule needs to be added first.
-        "$searchSlug/page/([0-9]{1,})/?$" => 'index.php?s=&paged=$matches[1]',
+        "$searchBase/$paginationBase/([0-9]{1,})/?$" => 'index.php?s=&paged=$matches[1]',
         // Make sure that an empty search is handled with the search slug.
-        "$searchSlug/?$" => 'index.php?s=',
+        "$searchBase/?$" => 'index.php?s=',
     ];
 
-    // Replace the search slug in the rewrite rules, but keep the original search rewrite rules.
     foreach ($searchRewrite as $key => $value) {
         if (!isset($newSearchRewriteRules[$key])) {
-            $newSearchRewriteRules[str_replace('search', $searchSlug, $key)] = $value;
+            $newSearchRewriteRules[str_replace($searchBase, $searchBase, $key)] = $value;
         }
     }
 
